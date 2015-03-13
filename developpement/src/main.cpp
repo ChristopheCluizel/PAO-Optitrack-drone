80f781a38e2c7a31ced436b4d286b23720786f0e
ChristopheCluizel <christophe.cluizel@gmail.com>
2015-03-13 17:21:58 +0100

    Prise en compte de l'aterrisage dans le signal ctrl+c.

--- developpement/src/main.cpp
+++ developpement/src/main.cpp
@@ -12,30 +12,29 @@
 using namespace std;
 
 ARdrone* drone;
+bool urgence = 0;
+
 void my_handler(int s){
-           printf("Arret D'urgence\n",s);
+		if(urgence)
+		{
+           printf("Arret D'urgence\n");
 		   drone->clearEmergencySignal();
+		}
+		else
+		{
+            printf("Atterissage\n");
+			drone->land();
+			urgence=1;
+			signal(SIGINT,my_handler);
+		}
+		   
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	signal(SIGINT,my_handler);
+	signal(SIGINT,my_handler);// gestion de l'arret d'urgence
 	
-	drone = new ARdrone();
-	drone->connect();
-	drone->config();
-	//drone->clearEmergencySignal();
-	drone->flatTrim();
-	drone->takeOff();
-	Sleep(6000);
-	drone->move(0,-0.1,0,0);
-	Sleep(1000);
-	drone->move(0,0.1,0,0);
-	Sleep(1000);
-	drone->land();
-	drone->disconnect();
-
-	/*ClientOptiTrack *client = new ClientOptiTrack();
+	ClientOptiTrack *client = new ClientOptiTrack();
 	int resultat = client->CreateClient(ConnectionType_Multicast);
 	if(resultat != ErrorCode_OK)
     {
@@ -47,12 +46,34 @@ int _tmain(int argc, _TCHAR* argv[])
     {
         printf("Client initialized and ready.\n");
     }
+
+	/*drone = new ARdrone();
+	drone->connect();
+	drone->config();
+	//drone->clearEmergencySignal();
+	drone->flatTrim();
+	drone->takeOff();*/
+	
+	
+	/*Sleep(6000);
+	drone->move(0,-0.1,0,0);
+	Sleep(1000);
+	drone->move(0,0.1,0,0);
+	Sleep(1000);
+	drone->land();
+	drone->disconnect();*/
+
+	while(1)
+	{
+
+	}
+	
 	Sleep(3000);
 	cout << client->getRigidBody(1).x << endl;
 	Sleep(3000);
 	cout << client->getRigidBody(1).x << endl;
 
-	Sleep(3000);*/
+	Sleep(3000);
 
 	return 0;
 }
